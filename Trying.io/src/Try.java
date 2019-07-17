9409c465ca39a5e13a870b1bc8986cf5635b9c41
majd <majd1999ziad@gmail.com>
2019-07-17 19:34:15 +0300

    Added member variables ,constructor,setters and getterrs

--- Trying.io/src/Try.java
+++ Trying.io/src/Try.java
@@ -1,3 +1,6 @@
+
+import java.util.ArrayList;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -9,5 +12,29 @@
  * @author majd1
  */
 public class Try {
+    private String userText;
+    private  ArrayList<String> errors;
+
+    public Try(String userText, ArrayList<String> errors) {
+        this.userText = userText;
+        this.errors = errors;
+    }
+
+    public String getUserText() {
+        return userText;
+    }
+
+    public void setUserText(String userText) {
+        this.userText = userText;
+    }
+
+    public ArrayList<String> getErrors() {
+        return errors;
+    }
+
+    public void setErrors(ArrayList<String> errors) {
+        this.errors = errors;
+    }
+    
     
 }
