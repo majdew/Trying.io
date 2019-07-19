219af534d937f222cb2594203f41e3cc1ad9c4fa
majd <majd1999ziad@gmail.com>
2019-07-19 17:45:46 +0300

    created basic class

---
+++ Trying.io/src/trying/io/BasicClass.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trying.io;
+
+
+
+/**
+ *
+ * @author majd1
+ */
+
+
+//This class for dealing with text written by the user and list of errors from the GUI application
+
+
+public class BasicClass {
+ 
+    
+}
