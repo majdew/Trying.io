2a5a10ddc9a05d860a27afa2657262d7c5f983b3
majd <majd1999ziad@gmail.com>
2019-07-19 22:04:21 +0300

    added the code to exit the form

--- Trying.io/src/trying/io/BasicMenuForm.java
+++ Trying.io/src/trying/io/BasicMenuForm.java
@@ -110,7 +110,9 @@ public class BasicMenuForm extends javax.swing.JFrame {
     }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
-        // TODO add your handling code here:
+     // this method to close the window
+     System.exit(0);
+        
     }//GEN-LAST:event_jButton3ActionPerformed
 
     /**
