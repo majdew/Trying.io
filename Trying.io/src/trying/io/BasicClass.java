b3669cd5b1761e4a8b32c87165de35bb3c960fd3
majd <majd1999ziad@gmail.com>
2019-07-26 15:44:47 +0300

    update the constructor for initializing  all new member variables

--- Trying.io/src/trying/io/BasicClass.java
+++ Trying.io/src/trying/io/BasicClass.java
@@ -30,15 +30,34 @@ public class BasicClass {
     private int numberOfWordsPerMin;
     
     
+     // this method is used to create object of the basic class intialize the member no variables
+    
+        public BasicClass() {
+        this.userTypedChars=new ArrayList<>();
+        this.charErrors=new HashMap<>();
+        this.numberOfUncorrectChars =0;
+        this.numberOfWordsPerMin=0;
+        this.numberOfTypedChars=0;
+       
+    }
+        
+        
     // method for return all typed characters 
+        
     public ArrayList<Character> getUserTypedChars() {
         return userTypedChars;
     }
+    
+    
     // method for assign value to the member variable userTypedChars
+    
     public void setUserTypedChars(ArrayList<Character> userTypedChars) {
         this.userTypedChars = userTypedChars;
     }
+    
+    
     // method that returns all uncorected errors as keys and values , key represent uncorected character and value number of error for each character
+   
     public String getCharErrors() {
         String errorList="";
         for(Character key : charErrors.keySet())
@@ -46,7 +65,9 @@ public class BasicClass {
         return errorList;          
     }
     
+    
     // method for  setting list of errors the error as key and value that takes uncorrected char (key) and  check if the character exist in the map it increases the value by one else assigns the number of error equal 1
+  
     public void setCharErrors(char errorChar) {
         if(charErrors.containsKey(errorChar)){
              int numberOfError =charErrors.get(errorChar);
@@ -57,47 +78,54 @@ public class BasicClass {
         } 
     }
     
+    
     //method that return integer represent number of typed characters takes no parameter
+   
     public int getNumberOfTypedChars() {
         return numberOfTypedChars;
     }
-
+    
     
     // method  that takes intger value as parameter represent number of typed characters and there is no return value
+    
     public void setNumberOfTypedChars(int numberOfTypedChars) {
         this.numberOfTypedChars = numberOfTypedChars;
     }
     
     
     // method returns integer value represent number of uncorrected characters and takes no parameter 
+   
     public int getNumberOfUncorrectChars() {
         return numberOfUncorrectChars;
     }
 
     
     // method that takes integer value represent number of uncorrected characters and there is no return value
+   
     public void getNumberOfUncorrectChars(int NumberOfUncorrectChars) {
         this.numberOfUncorrectChars=numberOfUncorrectChars;
     }
     
     // method that returns integer value represent number of words per minute  and takes no parameter
+    
     public int getNumberOfWordsPerMin() {
         return numberOfWordsPerMin;
     }
+    
 
     //method for setting number of words per minute  using number of typed characters and number of uncorrected characters  no return value  
+    
     public void setNumberOfWordsPerMin() {
         this.numberOfWordsPerMin = (numberOfTypedChars/5)-numberOfUncorrectChars;
     }
+    
+    
 
     
     
     
-    // this method is used to create object of the basic class 
-    public BasicClass(String userText, ArrayList<String> listOFError) {
  
-        
-    }
+ 
     
  
 
