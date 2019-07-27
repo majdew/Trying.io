e9fe191eb370e91710b301cfce107f108976514e
majd <majd1999ziad@gmail.com>
2019-07-27 17:35:51 +0300

    reset the remaining time

--- Trying.io/src/trying/io/PracticeForm.java
+++ Trying.io/src/trying/io/PracticeForm.java
@@ -201,7 +201,7 @@ public class PracticeForm extends javax.swing.JFrame {
                 
                 //check if the remaining time = 0  compare  user typed characters with original text
                 if(timeRemain==0){
-                    
+                    timeRemain=59; // reset the remaining time
                 }
 
                     
